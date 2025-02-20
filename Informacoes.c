baa3fefec4fed039c17f63f2e4356a64056b7be7
GabrielaProgramacao <gabriela.fernandesbatista1909@gmail.com>
2025-02-20 20:12:16 -0300

    Update Informacoes.c

--- Informacoes.c
+++ Informacoes.c
@@ -1,1 +1,42 @@
+#include <stdio.h>
 
+int main(){
+
+    int idade;
+    float altura;
+    double peso;
+    char letra;
+    char nome[30];
+  
+    printf("Digite sua idade: \n");
+    
+    scanf("%d", &idade);
+
+    printf("Sua idade é: %d\n");
+
+    printf("Digite sua altura: \n");
+    
+    scanf("%f", &altura");
+
+    printf("Sua altura é: %.2f\n");
+
+    printf("Digite seu peso: \n");
+
+    scanf("%f", &peso);
+
+    printf("Seu peso é: %.3f\n"); 
+
+    printf("Escolha uma letra: \n")  
+
+    scanf("%c", &letra);
+      
+    printf("Sua letra é: %c\n");
+
+    prinf("Seu nome é: %s \n");
+
+    scanf("%s", nome);
+
+    printf("Seu nome é: %s\n");
+
+    
+}
